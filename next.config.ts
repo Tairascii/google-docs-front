bd8a30b4ade26850d7dfe9bde11680b5549f502f
Tair Sairanbekov <tair.sairanbekov@tele2.kz>
2025-01-03 18:55:09 +0500

    feat(GD-1): init setup

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,20 @@
 import type { NextConfig } from "next";
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+
+const __dirname = path.dirname(__filename);
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  sassOptions: {
+    includePaths: [path.join(__dirname, "src/styles")],
+    prependData: `
+      @import "mixins.scss";
+      @import "vars.scss";
+      @import "extends.scss";
+    `,
+  },
 };
 
 export default nextConfig;
