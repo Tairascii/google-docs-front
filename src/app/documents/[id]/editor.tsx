54d234bebdb5f6e5f7bef8d68a9ad2124bba39b0
Tair Sairanbekov <tairtopskiy@gmail.com>
2025-01-04 16:59:34 +0500

    feat(GD-4): extension added

--- src/app/documents/[id]/editor.tsx
+++ src/app/documents/[id]/editor.tsx
@@ -2,18 +2,51 @@
 
 import { useEditor, EditorContent } from '@tiptap/react'
 import StarterKit from '@tiptap/starter-kit'
+import TaskItem from '@tiptap/extension-task-item'
+import TaskList from '@tiptap/extension-task-list'
+import Table from '@tiptap/extension-table'
+import TableCell from '@tiptap/extension-table-cell'
+import TableHeader from '@tiptap/extension-table-header'
+import TableRow from '@tiptap/extension-table-row'
+import Image from '@tiptap/extension-image'
+import ImageResize from 'tiptap-extension-resize-image'
 import styles from './editor.module.scss'
 
 export const Editor = () => {
   const editor = useEditor({
-    extensions: [StarterKit],
+    extensions: [
+      Image,
+      TaskItem.configure({ nested: true }),
+      TaskList,
+      Table,
+      TableCell,
+      TableHeader,
+      TableRow,
+      ImageResize,
+      StarterKit,
+    ],
     editorProps: {
       attributes: {
         style: 'padding-right: 56px; padding-left: 56px;',
         class: styles.editor,
       },
     },
-    content: '<p>Hello World! 🌎️</p>',
+    content: `
+        <table>
+          <tbody>
+            <tr>
+              <th>Name</th>
+              <th colspan="3">Description</th>
+            </tr>
+            <tr>
+              <td>Cyndi Lauper</td>
+              <td>Singer</td>
+              <td>Songwriter</td>
+              <td>Actress</td>
+            </tr>
+          </tbody>
+        </table>
+      `,
   })
 
   return (
