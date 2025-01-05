2997770319a226d4372323b2eecda7d52963ecb7
Tair Sairanbekov <tairtopskiy@gmail.com>
2025-01-05 12:35:22 +0500

    feat(GD-6): simple tools added

--- src/app/components/Editor/Editor.tsx
+++ src/app/components/Editor/Editor.tsx
@@ -10,6 +10,7 @@ import TableHeader from '@tiptap/extension-table-header'
 import TableRow from '@tiptap/extension-table-row'
 import Image from '@tiptap/extension-image'
 import ImageResize from 'tiptap-extension-resize-image'
+import Underline from '@tiptap/extension-underline'
 import { useEditorStore } from '@/app/store/useEditorStore'
 import styles from './Editor.module.scss'
 
@@ -51,6 +52,7 @@ export const Editor = () => {
       TableRow,
       ImageResize,
       StarterKit,
+      Underline,
     ],
     editorProps: {
       attributes: {
--- src/app/components/Toolbar/Toolbar.tsx
+++ src/app/components/Toolbar/Toolbar.tsx
@@ -1,7 +1,18 @@
 'use client'
 
 import { Section } from './types'
-import { Undo2Icon } from 'lucide-react'
+import {
+  BoldIcon,
+  ItalicIcon,
+  ListTodoIcon,
+  MessageSquarePlusIcon,
+  PrinterIcon,
+  Redo2Icon,
+  RemoveFormattingIcon,
+  SpellCheckIcon,
+  UnderlineIcon,
+  Undo2Icon,
+} from 'lucide-react'
 import { ToolbarButton } from '@/app/ui/ToolbarButton'
 import { useEditorStore } from '@/app/store/useEditorStore'
 import styles from './Toolbar.module.scss'
@@ -15,10 +26,92 @@ export const Toolbar = () => {
       icon: Undo2Icon,
       onClick: () => editor?.chain().undo().run(),
     },
+    {
+      id: 'redo',
+      label: 'Redo',
+      icon: Redo2Icon,
+      onClick: () => editor?.chain().redo().run(),
+    },
+    {
+      id: 'print',
+      label: 'Print',
+      icon: PrinterIcon,
+      onClick: () => window.print(),
+    },
+    {
+      id: 'spellCheck',
+      label: 'SpellCheck',
+      icon: SpellCheckIcon,
+      onClick: () => {
+        const current = editor?.view.dom.getAttribute('spellcheck')
+        editor?.view.dom.setAttribute(
+          'spellcheck',
+          current === 'false' ? 'true' : 'false'
+        )
+      },
+    },
   ]
+
+  const fontTools: Section[] = [
+    {
+      id: 'bold',
+      label: 'Bold',
+      icon: BoldIcon,
+      onClick: () => editor?.chain().toggleBold().run(),
+      isActive: editor?.isActive('bold'),
+    },
+    {
+      id: 'italic',
+      label: 'Italic',
+      icon: ItalicIcon,
+      onClick: () => editor?.chain().toggleItalic().run(),
+      isActive: editor?.isActive('italic'),
+    },
+    {
+      id: 'underline',
+      label: 'Underline',
+      icon: UnderlineIcon,
+      onClick: () => editor?.chain().toggleUnderline().run(),
+      isActive: editor?.isActive('underline'),
+    },
+  ]
+
+  const mediaTools: Section[] = [
+    {
+      id: 'comment',
+      label: 'Comment',
+      icon: MessageSquarePlusIcon,
+      onClick: () => {
+        console.log('comment')
+      },
+      isActive: false,
+    },
+    {
+      id: 'listTodo',
+      label: 'ListTodo',
+      icon: ListTodoIcon,
+      onClick: () => editor?.chain().focus().toggleTaskList().run(),
+      isActive: editor?.isActive('taskList'),
+    },
+    {
+      id: 'removeFormatting',
+      label: 'RemoveFormatting',
+      icon: RemoveFormattingIcon,
+      onClick: () => editor?.chain().focus().unsetAllMarks().run(),
+    },
+  ]
+
   return (
     <div className={styles.container}>
       {baseTools.map((item) => (
+        <ToolbarButton key={item.id} {...item} />
+      ))}
+      <hr className={styles.separator} />
+      {fontTools.map((item) => (
+        <ToolbarButton key={item.id} {...item} />
+      ))}
+      <hr className={styles.separator} />
+      {mediaTools.map((item) => (
         <ToolbarButton key={item.id} {...item} />
       ))}
     </div>
